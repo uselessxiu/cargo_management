7319b2c5d71df2ceb1ee383892aa91c8a8e67807
uselessxiu <hazarirajarshee777@gmail.com>
2025-04-05 10:53:47 +0530

    Create ProfileIcon.jsx

---
+++ ProfileIcon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const ProfileIcon = () => (
+  <svg
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="profile-icon"
+    style={{ width: "100px", height: "100px", marginRight: "20px" }}
+  >
+    <circle cx="50" cy="50" r="50" fill="#D9D9D9" />
+    <path
+      d="M66 68V64C66 61.8783 65.1571 59.8434 63.6569 58.3431C62.1566 56.8429 60.1217 56 58 56H42C39.8783 56 37.8434 56.8429 36.3431 58.3431C34.8429 59.8434 34 61.8783 34 64V68M58 40C58 44.4183 54.4183 48 50 48C45.5817 48 42 44.4183 42 40C42 35.5817 45.5817 32 50 32C54.4183 32 58 35.5817 58 40Z"
+      stroke="#1E1E1E"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
