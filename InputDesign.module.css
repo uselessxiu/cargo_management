16dc686f652b875a776739f2e81fadeb8ae028f8
Rudrajyoty Mahata <rudrajyotymahata98@gmail.com>
2025-04-05 12:01:29 +0530

    Create InputDesign.module.css

---
+++ InputDesign.module.css
@@ -0,0 +1,108 @@
+.mainContainer {
+  min-height: screen;
+  background-color: #000;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+}
+
+.mainTitle {
+  color: #fff;
+  text-align: center;
+  margin-bottom: 60px;
+  font-family: Inter;
+  font-size: 96px;
+  font-weight: 900;
+  line-height: 92px;
+}
+
+.contentSection {
+  z-index: 10;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  position: relative;
+}
+
+.gridContainer {
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  gap: 40px;
+  width: 100%;
+  max-width: 1840px;
+  display: grid;
+}
+
+.actionCard {
+  background-color: #3a3a3a;
+  border-radius: 50px;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  display: flex;
+}
+
+.cardGroup {
+  gap: 20px;
+  display: flex;
+}
+
+.cardContent {
+  flex-direction: column;
+  align-items: center;
+  display: flex;
+}
+
+.iconContainer {
+  background-color: #323232;
+  border-radius: 50px;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  display: flex;
+}
+
+.cardLabel {
+  color: #fff;
+  text-align: center;
+  margin-top: 33px;
+  font-family: Inter;
+  font-size: 20px;
+}
+
+.doubleCard {
+  grid-column: span 1;
+}
+
+@media (width <= 991px) {
+  .mainTitle {
+    font-size: 72px;
+    line-height: 70px;
+  }
+
+  .gridContainer {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    gap: 30px;
+  }
+}
+
+@media (width <= 640px) {
+  .mainTitle {
+    margin-bottom: 40px;
+    font-size: 48px;
+    line-height: 46px;
+  }
+
+  .gridContainer {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    gap: 20px;
+  }
+}
